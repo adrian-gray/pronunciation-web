42dc5613f1dbf9c020481b2a315fe31604bf5506
Adrian Gray <adrian@swoon.com.au>
2017-11-04 13:28:21 +1100

    added initial content

--- src/js/pages/Landing.jsx
+++ src/js/pages/Landing.jsx
@@ -3,7 +3,161 @@ import React from 'react'
 export default function Landing () {
   return (
     <div className='container'>
-      <h1>Hello World</h1>
+
+      <h1><span className='em'>/æ/</span> - short <span className='em'>a</span> ‘b<span className='em'>a</span>d’</h1>
+
+      <p>pronouncing /æ/ animation</p>
+
+      <h3>How to pronounce</h3>
+      <ul>
+        <li>jaw drops (quite a bit)</li>
+        <li>mouth opens wide</li>
+        <li>tip of the tongue stays forward – touching the back of the bottom front teeth (tongue tip is down)</li>
+        <li className='hilite'>back part of the tongue stretches up (back part lifts)</li>
+        <li>the tongue is wide</li>
+      </ul>
+
+
+      <h3>How to pronounce /æ/</h3>
+      <p>movie</p>
+
+
+      <table className='table table-sm'>
+        <thead>
+          <tr>
+            <th colSpan='2'>Words</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>b<span className='em'>a</span>d</td>
+            <td>/b<span className='em'>æ</span>d/</td>
+          </tr>
+          <tr>
+            <td>c<span className='em'>a</span>tch</td>
+            <td>/k<span className='em'>æ</span>tʃ/</td>
+          </tr>
+          <tr>
+            <td>th<span className='em'>a</span>nk</td>
+            <td>/θ<span className='em'>æ</span>ŋk/</td>
+          </tr>
+          <tr>
+            <td>h<span className='em'>a</span>nging</td>
+            <td>/h<span className='em'>æ</span>ŋɪŋ/</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <table className='table table-sm'>
+        <thead>
+          <tr>
+            <th colSpan='2'>Phrases</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>I’m not b<span className='em'>a</span>d.</td>
+            <td>/b<span className='em'>æ</span>d/</td>
+          </tr>
+          <tr>
+            <td>I didn’t c<span className='em'>a</span>tch that.</td>
+            <td>/k<span className='em'>æ</span>tʃ/</td>
+          </tr>
+          <tr>
+            <td>I’m good, th<span className='em'>a</span>nks.</td>
+            <td>/θ<span className='em'>æ</span>ŋk/</td>
+          </tr>
+          <tr>
+            <td>I’m h<span className='em'>a</span>nging in there.</td>
+            <td>/h<span className='em'>æ</span>ŋɪŋ/</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h3>phrases with /æ/ video</h3>
+      <p>video</p>
+
+      <h3>Short Dialogues</h3>
+      <p>‘Hi, I’m James. What’s your name?’<br />
+        ‘Hi, James, I’m <span className='em'>A</span>nne.’<br />
+        ‘Sorry, I didn’t c<span className='em'>a</span>tch that.’<br />
+        ‘<span className='em'>A</span>nne.’<br />
+        ‘Oh, <span className='em'>A</span>nne. Pleased to meet you. <span className='em'>A</span>nne.’<br />
+        ‘Pleased to meet you too.’</p>
+
+      <p>‘Hi. How are you?’<br />
+        ‘Good, th<span className='em'>a</span>nks. And you?’<br />
+        ‘Fine, th<span className='em'>a</span>nks.’</p>
+
+      <p>‘Hi. How are you going?’<br />
+        ‘Not b<span className='em'>a</span>d, th<span className='em'>a</span>nks. How about you?’<br />
+        ‘I’m good.’</p>
+
+      <p>Hi. How’s it going?’<br />
+        ‘Great. And you?’<br />
+        ‘I’m h<span className='em'>a</span>nging in there.’</p>
+
+
+      <h3>dialogues with /æ/ video</h3>
+      <p>video</p>
+
+      <table className='table table-sm'>
+        <thead>
+          <tr>
+            <th colSpan='2'>Common Words</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>c<span className='em'>a</span>sh</td>
+            <td>tr<span className='em'>a</span>sh</td>
+            <td>tr<span className='em'>a</span>nsfer</td>
+          </tr>
+          <tr>
+            <td>st<span className='em'>a</span>nd</td>
+            <td>ch<span className='em'>a</span>t</td>
+            <td>tr<span className='em'>a</span>ffic</td>
+          </tr>
+          <tr>
+            <td>b<span className='em'>a</span>nk</td>
+            <td>st<span className='em'>a</span>mp</td>
+            <td>m<span className='em'>a</span>tter</td>
+          </tr>
+          <tr>
+            <td>cr<span className='em'>a</span>sh</td>
+            <td>t<span className='em'>a</span>x</td>
+            <td>t<span className='em'>a</span>blet</td>
+          </tr>
+          <tr>
+            <td>c<span className='em'>a</span>b</td>
+            <td>sn<span className='em'>a</span>p</td>
+            <td>tr<span className='em'>a</span>nsport</td>
+          </tr>
+          <tr>
+            <td>pr<span className='em'>a</span>ctice</td>
+            <td>l<span className='em'>a</span>nguage</td>
+            <td>S<span className='em'>a</span>turday</td>
+          </tr>
+          <tr>
+            <td>ch<span className='em'>a</span>llenge</td>
+            <td>c<span className='em'>a</span>mera</td>
+            <td>m<span className='em'>a</span>nager</td>
+          </tr>
+          <tr>
+            <td>s<span className='em'>a</span>lad</td>
+            <td>p<span className='em'>a</span>ckage</td>
+            <td>c<span className='em'>a</span>ncel</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h3>Words maze /æ/</h3>
+      <ul>
+        <li>words words words words words</li>
+        <li>words words words words words</li>
+        <li>words words words words words</li>
+        <li>words words words words words</li>
+      </ul>
     </div>
   )
 }
