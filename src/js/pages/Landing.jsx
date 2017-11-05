ffaef8bfacdd5c2bbd66473102182ff2efbd1189
Adrian Gray <adrian@swoon.com.au>
2017-11-05 13:11:34 +1100

    added initial content

--- src/js/pages/Landing.jsx
+++ src/js/pages/Landing.jsx
@@ -17,10 +17,8 @@ export default function Landing () {
         <li>the tongue is wide</li>
       </ul>
 
-
       <h3>How to pronounce /æ/</h3>
       <p>movie</p>
-
 
       <table className='table table-sm'>
         <thead>
@@ -97,7 +95,6 @@ export default function Landing () {
         ‘Great. And you?’<br />
         ‘I’m h<span className='em'>a</span>nging in there.’</p>
 
-
       <h3>dialogues with /æ/ video</h3>
       <p>video</p>
 
@@ -151,13 +148,187 @@ export default function Landing () {
         </tbody>
       </table>
 
-      <h3>Words maze /æ/</h3>
+      <hr />
+      <h2>Member section</h2>
+
+      <h3>Tongue twisters</h3>
       <ul>
-        <li>words words words words words</li>
-        <li>words words words words words</li>
-        <li>words words words words words</li>
-        <li>words words words words words</li>
+        <li>The black bat had a bad back.</li>
+        <li>I’m chatting with the man at Chatime, Chatswood.</li>
+        <li>Canyoucanacanasa canner can can a can?</li>
       </ul>
+
+      <h3>Find the words</h3>
+      <h4>Click (✓) the words with the ‘short a’ <spane className='em'>/æ/</spane> sound</h4>
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>cap</div>
+          <div className='col'>hat</div>
+          <div className='col'>bug</div>
+          <div className='col'>cup</div>
+          <div className='col'>sad</div>
+          <div className='col'>hut</div>
+          <div className='col'>bag</div>
+          <div className='col'>hate</div>
+          <div className='col'>said</div>
+          <div className='col'>hot</div>
+        </div>
+      </div>
+
+      <h3>Hear the words</h3>
+      <h4>Underline the ‘short a’ /æ/ sound you hear</h4>
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>matter</div>
+          <div className='col'>mutter</div>
+          <div className='col'>pain</div>
+          <div className='col'>pan</div>
+          <div className='col'>paint</div>
+          <div className='col'>type</div>
+          <div className='col'>tap</div>
+          <div className='col'>top</div>
+          <div className='col'>ankle</div>
+          <div className='col'>uncle</div>
+        </div>
+      </div>
+
+      <h3><em>Say</em> the sentences below</h3>
+      <ol>
+        <li>My uncle had a bad ankle.</li>
+        <li>Whatever you mutter doesn’t matter.</li>
+        <li>Just tap the top of the tape.</li>
+      </ol>
+
+      <h3>The odd one out</h3>
+      <h4>Circle the words that don’t have the ‘short a’ /æ/ sound</h4>
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>ran</div>
+          <div className='col'>answer</div>
+          <div className='col'>band</div>
+          <div className='col'>start</div>
+          <div className='col'>att</div>
+          <div className='col'>all</div>
+          <div className='col'>thanks</div>
+          <div className='col'>black</div>
+          <div className='col'>had</div>
+          <div className='col'>name</div>
+        </div>
+      </div>
+
+      <h3>Identify the sounds</h3>
+      <h4>Write the correct sound: /æ/ (the short ‘a’ sound) or /eɪ / (the long ‘a’ sound)</h4>
+      <h5>snake eats tongs</h5>
+      <p>A man <span className='em'>(........) </span>
+      from Australia <span className='em'>(........) </span>
+      has a snake <span className='em'>(........). </span>
+      He feeds it a rat <span className='em'>(........) </span>
+      . He holds the rat with tongs. The snake eats the tongs, too. This is dangerous <span className='em'>(........) </span>
+      for the snake. Doctors operate on it. They take <span className='em'>(........) </span>
+      out the tongs. The snake’s name <span className='em'>(........) </span>
+      is Winston. After the operation <span className='em'>(........) </span>
+      he gets better.</p>
+
+      <h3>Words maze /æ/</h3>
+
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>happy</div>
+          <div className='col'>weather</div>
+          <div className='col'>doctor</div>
+          <div className='col'>natural</div>
+          <div className='col'>thank</div>
+          <div className='col'>value</div>
+        </div>
+        <div className='row'>
+          <div className='col'>family</div>
+          <div className='col'>number</div>
+          <div className='col'>jacket</div>
+          <div className='col'>cancel</div>
+          <div className='col'>nothing</div>
+          <div className='col'>narrow</div>
+        </div>
+        <div className='row'>
+          <div className='col'>carry</div>
+          <div className='col'>scratch</div>
+          <div className='col'>travel</div>
+          <div className='col'>hundred</div>
+          <div className='col'>seven</div>
+          <div className='col'>sandwich</div>
+        </div>
+        <div className='row'>
+          <div className='col'>ticket</div>
+          <div className='col'>possible</div>
+          <div className='col'>building</div>
+          <div className='col'>gallery</div>
+          <div className='col'>traffic</div>
+          <div className='col'>happen</div>
+        </div>
+        <div className='row'>
+          <div className='col'>empty</div>
+          <div className='col'>couple</div>
+          <div className='col'>wrapped</div>
+          <div className='col'>fashion</div>
+          <div className='col'>problem</div>
+          <div className='col'>question</div>
+        </div>
+        <div className='row'>
+          <div className='col'>bread</div>
+          <div className='col'>customer</div>
+          <div className='col'>matter</div>
+          <div className='col'>dozen</div>
+          <div className='col'>member</div>
+          <div className='col'>often</div>
+        </div>
+        <div className='row'>
+          <div className='col'>ankle</div>
+          <div className='col'>transfer</div>
+          <div className='col'>gather</div>
+          <div className='col'>already</div>
+          <div className='col'>healthy</div>
+          <div className='col'>message</div>
+        </div>
+        <div className='row'>
+          <div className='col'>swam</div>
+          <div className='col'>heavy</div>
+          <div className='col'>said</div>
+          <div className='col'>candy</div>
+          <div className='col'>snatch</div>
+          <div className='col'>package</div>
+        </div>
+        <div className='row'>
+          <div className='col'>add</div>
+          <div className='col'>transport</div>
+          <div className='col'>dinner</div>
+          <div className='col'>salad</div>
+          <div className='col'>pleasure</div>
+          <div className='col'>apple</div>
+        </div>
+        <div className='row'>
+          <div className='col'>brother</div>
+          <div className='col'>rapid</div>
+          <div className='col'>sorry</div>
+          <div className='col'>expand</div>
+          <div className='col'>finish</div>
+          <div className='col'>salmon</div>
+        </div>
+        <div className='row'>
+          <div className='col'>level</div>
+          <div className='col'>national</div>
+          <div className='col'>wrong</div>
+          <div className='col'>catch</div>
+          <div className='col'>mother</div>
+          <div className='col'>ambulance</div>
+        </div>
+        <div className='row'>
+          <div className='col'>model</div>
+          <div className='col'>answer</div>
+          <div className='col'>shallow</div>
+          <div className='col'>salary</div>
+          <div className='col'>become</div>
+          <div className='col'>relax</div>
+        </div>
+      </div>
     </div>
   )
 }
