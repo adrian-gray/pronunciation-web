63f665d5756c76bb6ed68708fc40e8058b9e4b2b
Adrian Gray <adrian@swoon.com.au>
2018-08-19 14:05:33 +1000

    adding record when adding user

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,9 +1,25 @@
 import firebase from 'firebase/app'
 import { auth } from './firebase'
 
-export const handleGoogleLogin = () => {
-  const provider = new firebase.auth.GoogleAuthProvider()
+const setupNewUser = (user) => {
+  firebase.database().ref('users/' + user.uid).set({
+    'subscriptionLevel': 0
+  })
+}
+
+const checkForAssignNewRecord = (e) => {
+  if (e.additionalUserInfo.isNewUser) {
+    setupNewUser(e.user)
+  }
+}
+
+const signIn = (provider) => {
   auth.signInWithPopup(provider)
+    .then(checkForAssignNewRecord)
+}
+
+export const handleGoogleLogin = () => {
+  signIn(new firebase.auth.GoogleAuthProvider())
 }
 
 export const signOut = () => auth.signOut()
