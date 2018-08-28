d95cc7aee9ab3de09d618f2c6a6990cf807007cb
Adrian Gray <adrian@swoon.com.au>
2018-08-28 12:40:25 +1000

    landing page tweaks

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -12,9 +12,6 @@ import Mailchimp from './../components/Mailchimp'
 import SEO from './../components/SEO'
 
 const styles = (theme) => ({
-  center: {
-    textAlign: 'center'
-  },
   button: {
     marginTop: theme.spacing.unit,
     marginBottom: 2 * theme.spacing.unit,
@@ -27,8 +24,12 @@ const styles = (theme) => ({
     paddingTop: '0.5em',
     paddingBottom: '0.5em'
   },
-  fullWidth: {
+  video: {
     width: '100%'
+  },
+  startBtn: {
+    textAlign: 'center',
+    padding: '2em'
   }
 })
 
@@ -45,30 +46,28 @@ const Landing = (props) => {
         {'Improve Your Spoken English'}
       </Typography>
       <Typography className={classes.para} gutterBottom>
-        {'Is poor English pronunciation holding you back in your career or studies? Do you want to improve your spoken English, but are not sure how? We are here to help you.'}
+        {'Is poor English pronunciation holding you back in your career or studies? Do you want to improve your spoken English, but are not sure how? Help is on the way!'}
       </Typography>
       <Typography className={classes.para} gutterBottom>
-        {'We have English pronunciation video lessons from a registered English pronunciation instructor. We follow the pronunciation lessons with English pronunciation examples, recorded conversations, and interactive activities to help improve your pronunciation the right way. Learn how to improve your English pronunciation to speak clearly and professionally.'}
+        {'We are currently producing over 60 videos lessons to help you improve your spoken English. The videos feature Nicky Brooks, a professional English language consultant, giving English language pronunciation lessons made for this site. She teaches how to make the sounds used in English, how to understand English sounds, and how to improve your accent in English.'}
       </Typography>
-      <Typography className={classes.para}>
-        {'Take your English pronunciation to the next level'}
+      <Typography className={classes.para} gutterBottom>
+        {'Following the pronunciation lessons are English pronunciation examples, recorded conversations, and interactive activities to help improve your pronunciation the right way. We are adding currently adding sounds and activities weekly. Improve your English pronunciation to speak clearly and professionally.'}
       </Typography>
-      <hr className='my-4' />
       <Typography variant='headline' gutterBottom>
-        {'Be in the know as soon as we launch'}
+        {'Stay up to date as we add pronunciation activities'}
       </Typography>
       <Typography className={classes.headspace} gutterBottom>
-        {'We are currently adding sounds and activities multiple times a week and are eopn in a limited testing capacity. We aim to launch in late 2018.'}
+        {'We are currently adding sounds and activities multiple times a week and are open in a limited testing capacity. We aim to launch in late 2018.'}
       </Typography>
       <Mailchimp />
-      <hr className='my-4' />
-      <div className={classes.center}>
+      <div className={classes.startBtn}>
         <Button variant='contained' size='large' className={classes.button} component={Link} to='/home'>
           {'Start Pronunciation Practice'}
         </Button>
       </div>
       <img
-        className={classes.fullWidth}
+        className={classes.video}
         src='/assets/images/improve-english-pronounciation.jpg'
         title='Learn to improve English pronunciation'
         alt='Learn to improve English pronunciation'
