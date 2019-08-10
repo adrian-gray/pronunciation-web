7a4e9cb4eea67bf56ddbb83c95ac96b7e9dcb4d7
Adrian Gray <adrian@swoon.com.au>
2019-08-10 15:56:44 +1000

    Added SEO for 'long u'

---
+++ src/data/seo/long_u.js
@@ -0,0 +1,91 @@
+// title 65-75
+//  title: `##########**********##########**********##########**********@@@@@----------
+// description 150 - 170
+//  description: `##########**********##########**********##########++++`
+
+export default {
+  "sound long_u none": {
+    title:
+      "How to pronounce long ‘u’ in English. Lessons and interactive activities.",
+    canonical: `${URL}/sound/long-u`
+  },
+  "sound long-u how to pronounce": {
+    title:
+      "How to pronounce u:, the long ‘u’ in English. Description and images.",
+    canonical: `${URL}/sound/long-u/how-to-pronounce`
+  },
+  "sound long-u movie": {
+    title:
+      "Movie demonstrating the correct pronunciation of long ‘u’ - the u: sound.",
+    canonical: `${URL}/sound/long-u/movie`
+  },
+  "sound long-u words": {
+    title:
+      "Common English words using u:, the long ‘u’ sound, with phoenetic spelling",
+    canonical: `${URL}/sound/long-u/words`
+  },
+  "sound long-u phrases": {
+    title: "English language phrases using the long ‘u’, or phoenetic u: sound",
+    canonical: `${URL}/sound/long-u/phrases`
+  },
+  "sound long-u dialogues": {
+    title:
+      "Example English dialogues showing use of the long ‘u’, or u:, sound",
+    canonical: `${URL}/sound/long-u/dialogues`
+  },
+  "sound long-u common words": {
+    title:
+      "A list of common English words using the long ‘u’ sound, the IPA u: sound",
+    canonical: `${URL}/sound/long-u/common-words`
+  },
+  "sound long-u tongue twisters": {
+    title: "English language tongue twisters featuring long ‘u’, or u: sounds",
+    description:
+      "Enjoy the challenge of Tongue Twisters. Three tongue twisters to perfect the long ‘u’ sound. Try to say each one as many times as you can, no mistakes.",
+    canonical: `${URL}/sound/long-u/tongue-twisters`
+  },
+  "sound long-u minimal pairs": {
+    title:
+      "English minimal pairs pronunciation practice with long ‘u’, or u: sounds",
+    canonical: `${URL}/sound/long-u/minimal-pairs`
+  },
+  "sound long-u choose the sound": {
+    title:
+      "Pronunciation activity for long ‘u’. Choose the words with the u:, ʊ, or Λ sound",
+    description:
+      "Read the text below. Find the words which have an /u:/, /ʊ/ or /Λ/. Write the words in the table under the correct heading.",
+    canonical: `${URL}/sound/long-u/find-the-words`
+  },
+  "sound long-u fill in the spaces": {
+    title:
+      "Write one of the letters: u, oo, ou, or o in the gaps below to make a word.",
+    canonical: `${URL}/sound/long-u/fill-in-the-spaces`
+  },
+  "sound long-u listen to the sounds": {
+    title: "Select the words you hear using the long 'u' sound?",
+    description:
+      "Odd One Out helps you recognise and pronounce the long ‘u’ vowel sound of common English words. Can you find all the odd words before your time runs out?",
+    canonical: `${URL}/sound/long-u/listen-to-the-sounds`
+  },
+  "sound long-u beginner news stories": {
+    title:
+      "Beginner Identify the News Story sounds,  practice long and long ‘u’ sounds",
+    description:
+      "News Stories helps you to identify individual sounds in long and more difficult written text. This improves your pronunciation of the words you read and your reading fluency.",
+    canonical: `${URL}/sound/long-u/beginner-news-stories`
+  },
+  "sound long-u intermediate news stories": {
+    title:
+      "Intermediate Identify the News Story sounds,  practice long and long ‘u’ sounds",
+    description:
+      "News Stories helps you to identify individual sounds in long and more difficult written text. This improves your pronunciation of the words you read and your reading fluency.",
+    canonical: `${URL}/sound/long-u/intermediate-news-stories`
+  },
+  "sound long-u words maze": {
+    title:
+      "Solve the Words Maze puzzle - long ‘u’. long puactice English pronunciation with long ‘u’",
+    description:
+      "Maze is an interactive pronunciation game that helps you become familiar with common English words so you can speak with confidence.",
+    canonical: `${URL}/sound/long-u/words-maze`
+  }
+};
